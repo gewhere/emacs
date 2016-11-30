3fa758cee4a445f9855d4ac8f2aa66f1ae05d8cd
gewhere <georgios.diapoulis@gmail.com>
2016-12-01 00:41:30 +0200

    add default irc channels

--- init-org/init.el
+++ init-org/init.el
@@ -7,66 +7,6 @@
 (add-to-list 'load-path "~/PKGBUILDS/emacs-helm/src/helm-1.9.9/")
 (add-to-list 'load-path "~/sources/helm-projectile/")
 
-;; ;; https://tuhdo.github.io/helm-intro.html
-;; (require 'helm)
-;; (require 'helm-config)
-;;
-;; ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-;; (global-set-key (kbd "C-c h") 'helm-command-prefix)
-;; (global-unset-key (kbd "C-x c"))
-;;
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-;;
-;; (when (executable-find "curl")
-;;   (setq helm-google-suggest-use-curl-p t))
-;;
-;; (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-;;       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-;;       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-;;       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-;;       helm-ff-file-name-history-use-recentf t
-;;       helm-echo-input-in-header-line t)
-;;
-;; (defun spacemacs//helm-hide-minibuffer-maybe ()
-;;   "Hide minibuffer in Helm session if we use the header line as input field."
-;;   (when (with-helm-buffer helm-echo-input-in-header-line)
-;;     (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-;;       (overlay-put ov 'window (selected-window))
-;;       (overlay-put ov 'face
-;;                    (let ((bg-color (face-background 'default nil)))
-;;                      `(:background ,bg-color :foreground ,bg-color)))
-;;       (setq-local cursor-type nil))))
-;;
-;;
-;; (add-hook 'helm-minibuffer-set-up-hook
-;;           'spacemacs//helm-hide-minibuffer-maybe)
-;;
-;; (setq helm-autoresize-max-height 0)
-;; (setq helm-autoresize-min-height 20)
-;; (helm-autoresize-mode 1)
-;;
-;; (helm-mode 1)
-
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
-;; (global-set-key (kbd "C-x b") 'helm-mini)
-;; (setq helm-buffers-fuzzy-matching t
-;;       helm-recentf-fuzzy-match    t)
-;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
-;; (setq helm-locate-fuzzy-match t)
-;; (setq helm-lisp-fuzzy-completion t)
-
-;; (setq helm-projectile-fuzzy-match nil)
-;; (require 'helm-projectile)
-;; (helm-projectile-on)
-
 ;; THEMES
 (setq prelude-theme 'nil)
 
@@ -74,12 +14,11 @@
 (add-to-list 'custom-theme-load-path "~/sources/color-theme-tangotango")
 (load-theme 'tangotango t)
 
-
 ;; powerline
 (add-to-list 'load-path "~/sources/emacs-powerline")
 (require 'powerline)
 
-;; moe theme for powerline
+;; moe theme
 (add-to-list 'custom-theme-load-path "~/sources/moe-theme.el/")
 (add-to-list 'load-path "~/sources/moe-theme.el/")
 (require 'moe-theme)
@@ -128,7 +67,7 @@
 
 (setq rcirc-server-alist
       '(("irc.freenode.net" :port 6697 :encryption tls
-         :channels ("#hsgr-cc"))))
+         :channels ("#hsgr-cc" "#bash" "#supercollider" "##machinelearning"))))
 ;; "#python" "#bash" "#emacs" "#emacs-beginners" "#archlinux" "#org-mode" "##learnpython" "#archlinux-greece" "#tmux" "#xterm" "#systemd" "#git" "#haskell-beginners" "#regex" "##machinelearning" "#archlinux-offtopic" "##philosophy" "##statistics" "#musicbrainz" "#esoteric" "#music-theory" "##logic" "#ai" "##music" "#archlinux-aur" "##cs" "##matlab" "#octave" "#archlinux-pacman" "#supercollider"))))
 ;;(setq rcirc-server-alist
 ;;      '(("irc.freenode.net" :channels ("#hsgr-cc" "#python" "#bash" "#emacs" "#emacs-beginners" "#archlinux" "#rirc" "#org-mode" "##learnpython" "#archlinux-greece" "#archlinux-newbie"))))
@@ -308,7 +247,7 @@ href=\"http://sachachua.com/blog/wp-content/themes/sacha-v3/foundation/css/found
    (lilypond . t)
    (java . t)
    )
- )
+)
 
 (autoload 'run-octave "octave-inf" nil t)
 ;;(autoload 'octave-mode "octave-mod" nil t)
